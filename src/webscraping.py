80f8998f2a6c511ec9fd756d9e70b820445c99a3
AbhinavJoe <abhinavjoshi102003@gmail.com>
2023-12-19 14:03:25 +0530

    Update webscraping

--- src/webscraping.py
+++ src/webscraping.py
@@ -8,12 +8,12 @@ import re
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Obtain the version of ChromeDriver compatible with the browser being used.
-driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Which website to scrape
-val = input("Enter a url:")
+val = input("Enter a url: ")
 
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 5)
 
 driver.get(val)
 
@@ -26,30 +26,37 @@ if get_url == val:
     page_source = driver.page_source
 
 # Use BeautifulSoup to parse the HTML scraped from the webpage.
-soup = BeautifulSoup(page_source,features="html.parser")
+soup = BeautifulSoup(page_source, features="html.parser")
 
 # Parse the soup for User Input Keywords.
-multiple=input("Would you like to enter multiple keywords?(Y/N)")
+multiple = input("Would you like to enter multiple keywords?(Y/N)")
+
 
 if multiple == "Y":
-    keywords=[]
-    matches=[]
-    len_match=[]
-    num_keyword=input("How many keywords would you like to enter?")
-    count=int(num_keyword)
-while count != 0:
-    keyword=input("Enter a keyword to find instances of in the article:")
-    keywords.append(keyword)
-    match=soup.body.find_all(string=re.compile(keyword))
-    matches.append(match)
-    len_match.append(len(match))
-    count -= 1
-    df=pd.DataFrame({"Keyword":pd.Series(keywords),"Number of Matches": pd.Series(len_match),"Matches":pd.Series(matches)})
+    keywords = []
+    matches = []
+    len_match = []
+    num_keyword = input("How many keywords would you like to enter?")
+    count = int(num_keyword)
+    while count != 0:
+        keyword = input("Enter a keyword to find instances of in the article:")
+        keywords.append(keyword)
+        match = soup.body.find_all(string=re.compile(keyword))
+        matches.extend(match)  # Use extend to flatten the list
+        len_match.append(len(match))
+        count -= 1
+    df = pd.DataFrame({
+        "Keyword": pd.Series(keywords),
+        "Number of Matches": pd.Series(len_match),
+        "Matches": pd.Series(matches)
+    })
+
 elif multiple == "N":
-    keyword=input("Enter a keyword to find instances of in the article:")
+    keyword = input("Enter a keyword to find instances of in the article:")
     matches = soup.body.find_all(string=re.compile(keyword))
     len_match = len(matches)
-    df=pd.DataFrame({"Keyword":pd.Series(keyword),"Number of Matches": pd.Series(len_match), "Matches":pd.Series(matches)})
+    df = pd.DataFrame({"Keyword": pd.Series(keyword), "Number of Matches": pd.Series(
+        len_match), "Matches": pd.Series(matches)})
 else:
     print("Error, invalid character entered.")
 
