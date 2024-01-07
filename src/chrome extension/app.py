04918a995860332580ee0a0b48b90fe1a816aa6d
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-01-08 00:45:19 +0530

    Update app.py

--- src/chrome extension/app.py
+++ src/chrome extension/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('popup.html')
+
+
+@app.route('/submitnumber', methods=['POST'])
+def submit_number():
+    number = request.form['number']
+
+    # Process the form data as needed
+    # For example, you can print the data
+    print(f'Number: {number}')
+
+    # Redirect or render another template as needed
+    return render_template('result.html', number=number)
+
+
+@app.route('/submitfeedback', methods=['POST'])
+def submit_feedback():
+    feedbackurl = request.form['feedbackurl']
+    feedbacktext = request.form['feedbacktext']
+
+    # Process the form data as needed
+    # For example, you can print the data
+    print(f'Feedback URL: {feedbackurl}, Feedback Text: {feedbacktext}')
+
+    # Redirect or render another template as needed
+    return render_template('result.html', feedbackurl=feedbackurl, feedbacktext=feedbacktext)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
