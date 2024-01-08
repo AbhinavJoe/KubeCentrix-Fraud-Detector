12cdc7899dbec573bbb8ea77f85bb7087142505f
Divyanshu Kasherwal <112760132+kasherwal19@users.noreply.github.com>
2024-01-08 16:00:47 +0530

    Update app.py

--- src/chrome extension/app.py
+++ src/chrome extension/app.py
@@ -1,48 +1,35 @@
-from checkno import check_phone_number
 from flask import Flask, render_template, request
-import json
-import os
+from truecallerpy import search_phonenumber
 
 app = Flask(__name__)
 
-current_directory = os.path.dirname(os.path.realpath(__file__))
-json_file_path = os.path.join(
-    current_directory, "../../data/phonebook_data.json"
-)
+id = "a1i04--kE1GeYFb-hPQ7gmvIWvjV8hTQdI74aC1IDKiDcogB0zyFezzT0764fYMQ"
 
+async def check_truecaller(phone_number):
+    try:
+        result = await search_phonenumber({"phone_number": phone_number}, "IN", id)
+        contact_name = result['data']['data'][0]['name']
+        return f"{phone_number} is legit. This number belongs to {contact_name}."
+    except Exception as e:
+        print(f"Error: {e}")
+        return f"{phone_number} is suspicious. Unable to verify with Truecaller."
 
 @app.route('/')
 def index():
     return render_template('popup.html')
 
-
 @app.route('/submitnumber', methods=['POST'])
 def submit_number():
     phone_number = request.form['phone_number']
-
-    # # Process the form data as needed
-    # # For example, you can print the data
-    # print(f'Number: {phone_number}')
-
-    # # Redirect or render another template as needed
-    # return render_template('result.html', number=phone_number)
-
-    result = check_phone_number(phone_number)
-    return render_template('verification.html', result=result)
-
+    result = check_truecaller(phone_number)
+    return render_template('templates/verification.html', result=result)
 
 @app.route('/submitfeedback', methods=['POST'])
 def submit_feedback():
     website_url = request.form['website_url']
     feedback_text = request.form['feedback_text']
-
-    # Process the form data as needed
-    # For example, you can print the data
     print(f'Feedback URL: {website_url}, Feedback Text: {feedback_text}')
-
-    # Redirect or render another template as needed
-    return render_template('result.html', feedbackurl=website_url, feedbacktext=feedback_text)
-
+    return render_template('templates/feedback.html', feedbackurl=website_url, feedbacktext=feedback_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
