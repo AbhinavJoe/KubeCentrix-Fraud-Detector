4a5adb387854f5e8f6f08f768af248757b856190
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-01-10 01:46:38 +0530

    Update background.js

--- src/chrome extension/background.js
+++
@@ -1,7 +0,0 @@
-const serverUrl = 'http://localhost:5000';
-const socket = io.connect(serverUrl);
-
-socket.on('update', (data) => {
-    console.log('Received update:', data);
-    // Process the update as needed
-});
