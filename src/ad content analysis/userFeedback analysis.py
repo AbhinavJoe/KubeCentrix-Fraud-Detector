a18df23fe662440c8527ff5f7ed0385cc436c298
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-01-18 07:05:39 +0530

    Update userFeedback analysis.py

--- src/ad content analysis/userFeedback analysis.py
+++ src/ad content analysis/userFeedback analysis.py
@@ -11,16 +11,13 @@ def analyze_sentiment(text):
     return nlp(text)
 
 
-# Load the JSON file
 current_directory = os.path.dirname(os.path.realpath(__file__))
 json_file_path = os.path.join(
-    current_directory, "../../data/feedback_data.json")  # Replace with your JSON file path
+    current_directory, "../../data/feedback_data.json")
 
 with open(json_file_path, 'r') as file:
     data = json.load(file)
 
-# Assuming the JSON file contains a list of records
-# If it's a single record, you can directly access data['FeedbackText']
 for record in data:
     feedback = record['FeedbackText']
     sentiment_result = analyze_sentiment(feedback)
