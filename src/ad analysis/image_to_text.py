92ee0a0fd2f8eaf9c4f1a50bc519dbb509f86b04
AbhinavJoe <abhinavjoshi102003@gmail.com>
2024-02-13 15:19:49 +0530

    Rename image to text.py

--- src/ad analysis/image_to_text.py
+++ src/ad analysis/image_to_text.py
@@ -6,12 +6,12 @@ from io import BytesIO
 # URL of the image
 image_url = 'https://tpc.googlesyndication.com/simgad/1462447038842297607?'
 
-# Fetch the image from the URL
+# Fetchs the image from the URL
 response = requests.get(image_url)
 if response.status_code == 200:
     image = Image.open(BytesIO(response.content))
 
-    # Use Tesseract to do OCR on the image
+    # Uses Tesseract to do OCR on the image
     text = pytesseract.image_to_string(image)
     print(text)
 else:
